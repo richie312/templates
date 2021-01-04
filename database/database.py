a60d695dbd43c7184222f2f342fc83e028567dbf
richie312 <richie.chatterjee31@gmail.com>
2021-01-04 19:32:05 +0530

    added the initial files

---
+++ database/database.py
@@ -0,0 +1,16 @@
+''' Database config '''
+import json
+
+with open('database_auth.json','r') as readfile:
+    database_auth = json.load(readfile)
+host = database_auth['host']
+user = database_auth['dbuser']
+passwd = database_auth['db_pass']
+database = database_auth['dbname']
+
+
+def db_connection():
+    connection = pymysql.connect(host= host, user = user,
+                             password = passwd)
+    return connection
+
---
+++ test_api.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+
+import flask
+from flask import Flask, request, jsonify
+from flask import render_template
+import pymysql
+from database import db_connection
+import json
+app = Flask(__name__) 
+app.config['DEBUG'] = True
+
+connection = db_connection()
+
+""" Server Call Back Functions"""
+
+@app.route('/')
+def home():
+    return render_template('Resume.html')
+
+@app.route('/gor')
+def home():
+    return render_template('gor.html')
+
+@app.route('/framework_design')
+def home():
+    return render_template('pseudo2source.html')
+
+@app.route('/api_documentation')
+def home():
+    return render_template('skillz_doc.html')
+
+
+@app.route('/get_data',methods=['GET'])
+def submission_id():
+    cursor = connection.cursor()
+    cursor.execute("show columns from richie31.company_email1")
+    column_heads = cursor.fetchall()
+    columns = [column_heads[i][0] for i in range(len(column_heads))]
+    sql_query = "select * from richie31.company_email1;"
+    cursor.execute(sql_query)
+    email_list = cursor.fetchall()
+    response = {'columns': columns,
+                'data':email_list}
+    return jsonify(response)
+    
+
+if __name__ == '__main__':
+    app.run(host = "0.0.0.0",port = 5001, debug = True)
+    
